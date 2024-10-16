b39f519b94ac1154de251dcdfe7cc9118fa1f602
vargoscreator <bacahoriyk@gmail.com>
2024-10-16 12:09:15 +0300

    fix

--- js/checkout.js
+++ js/checkout.js
@@ -1,25 +1,24 @@
-const submitButton = document.querySelector('.order__block-send');
+const submitButton = document.querySelector('.checkout__block-send');
 submitButton.addEventListener('click', function(event) {
-    const requiredFields = document.querySelectorAll('input[required], textarea[required]');
+    const requiredFields = document.querySelectorAll('input[required]');
     let isValid = true;
-
     requiredFields.forEach(field => {
         if (field.value.trim() === '') {
-            field.style.borderColor = '#FF0000';
+            field.classList.add('error')
             isValid = false;
         } else {
-            field.style.borderColor = '';
+            field.classList.remove('error')
         }
     });
     if (!isValid) {
         event.preventDefault();
     }
 });
-const requiredFields = document.querySelectorAll('input[required], textarea[required]');
+const requiredFields = document.querySelectorAll('input[required]');
 requiredFields.forEach(field => {
     field.addEventListener('input', () => {
         if (field.value.trim() !== '') {
-            field.style.borderColor = '';
+            field.classList.remove('error')
         }
     });
 });
